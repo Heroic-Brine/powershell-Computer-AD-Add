9ee06be0deec28899f3c7212bdff1d766b74298d
Heroic-Brine <stefanotoft101112@live.dk>
2020-04-01 16:10:01 +0200

    Fixed spelling errors and removed test code

--- powershell-Computer-AD-Add.ps1
+++ powershell-Computer-AD-Add.ps1
@@ -3,16 +3,7 @@ param (
     [string] $DomainName = $(throw "-domainname is required"), 
     #the thorw is unessery due to the mandatory written above i just wanteted to try thorws
     [Parameter(Mandatory = $true)]
-    [string] $username = $(throw "-username is required") 
+    [string] $UserName = $(throw "-username is required") 
 )
-#Checks if script currently is administrator
-#pause
-#If (-NOT ([Securi-ty.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-#{
-#Starts the script as administrator
-Start-Process powershell.exe $PSCommandPath -Verb runAs
-#exit
-#} 
-
-
-Add-Computer -DomainName $DomainName -Credential $username
+Add-Computer -DomainName $DomainName -Credential $UserName
+Start-Sleep 1
